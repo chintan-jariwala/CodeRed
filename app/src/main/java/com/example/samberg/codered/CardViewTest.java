43671ddc7cd0715c27823b664de28f34193c33f1
stacya14 <amberg.stacy@cusd80.com>
2016-06-13 12:39:39 -0700

    added page for cardview test

---
+++ app/src/main/java/com/example/samberg/codered/CardViewTest.java
@@ -0,0 +1,13 @@
+package com.example.samberg.codered;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CardViewTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_view_test);
+    }
+}
